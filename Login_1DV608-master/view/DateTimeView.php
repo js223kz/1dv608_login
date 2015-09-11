35081e5290fc7cea97d58c123ceabacd960560ae
js223kz <js223kz@student.lnu.se>
2015-09-11 16:09:13 +0200

    Adding DateTime model

--- Login_1DV608-master/view/DateTimeView.php
+++ Login_1DV608-master/view/DateTimeView.php
@@ -5,7 +5,7 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$timeString = 'En superbra tid här';
 
 		return '<p>' . $timeString . '</p>';
 	}
